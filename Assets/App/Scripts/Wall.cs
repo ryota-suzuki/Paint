10d8eb12cc4849a35381e72c749ef5951b0f1eb7
鈴木亮太 <suzukiryouta@suzukirutanoair.lan>
2014-10-15 03:33:11 +0900

    trigger between player and wall

---
+++ Assets/App/Scripts/Wall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter (Collider Other) {
+		if (Other.gameObject.tag != "Player")
+			return;
+
+		Debug.Log ("test");
+		Destroy (Other.gameObject);
+	}
+}
